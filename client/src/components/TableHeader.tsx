c75b14450119158fb4a8b070923f5885e9d609df
gurvir dhillon <gman.dhillon@gmail.com>
2024-07-28 22:20:27 -0700

    semibolded action table header to match other headers

--- client/src/components/TableHeader.tsx
+++ client/src/components/TableHeader.tsx
@@ -7,7 +7,7 @@ const TableHeader: React.FC = () => {
         <span className="w-1/3 font-semibold">name</span>
         <span className="w-1/3 font-semibold">amount</span>
         <span className="w-1/3 font-semibold">date</span>
-        <span className="w-1/6">actions</span>
+        <span className="w-1/6 font-semibold">actions</span>
       </div>
     </div>
   );
